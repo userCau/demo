193710027984d85037d7a78ff8c9edacd32d2698
Cauã Carvalho <CauCauPlays@gmail.com>
2025-10-15 22:53:20 -0300

    Update ErroController.java

--- src/main/java/com/projeto/gestock/controller/ErroController.java
+++ src/main/java/com/projeto/gestock/controller/ErroController.java
@@ -8,6 +8,6 @@ public class ErroController {
 
     @GetMapping("/acessoNegado")
     public String acessoNegado() {
-        return "acessoNegado"; // nome do template (src/main/resources/templates/acesso-negado.html)
+        return "acessoNegado"; 
     }
 }
