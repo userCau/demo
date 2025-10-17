ab4b6fb023e2a45db864e99e243c2230261ea14e
Matheus Graciano Peres <mgracianoperes@icloud.com>
2025-10-17 00:20:51 -0300

    Estilização Dashboard

---
+++ src/main/java/com/projeto/gestock/security/testeEncrypt.java
@@ -0,0 +1,10 @@
+// package com.projeto.gestock.security;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+// public class testeEncrypt {
+//     public static void main(String[] args) {
+//         String senha = "jump";
+//         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//         System.out.println(encoder.encode(senha));
+//     }
+// }
